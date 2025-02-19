186427f61f266354bc47d3b5776503de773b8f54
rajdevweb <wordhello160@gmail.com>
2025-02-19 11:43:51 +0530

    done-first-submenu

--- script.js
+++ script.js
@@ -19,15 +19,30 @@ function openMenu(){
 }
 // let showItem = document.getElementById("showing-div").style.display = "block"
 // let noneItem  = document.getElementById("showing-div").style.display = "none"
-let Mybtn = document.getElementById("showBtn").onclick = function(){showHide()}
-function showHide(){
-   document.getElementById("showing-div").classList.toggle("show")
-}
+// let Mybtn = document.getElementById("showBtn").onclick = function(){showHide()}
+// function showHide(){
+//    document.getElementById("showing-div").classList.toggle("show")
+// }
 //  if(noneItem.style.display === "none"){
 //     document.getElementById("showing-div").style.display = "block"
 //     }else if(noneItem.style.display === "block"){
 //         document.getElementById("showing-div").style.display = "none"
 //     }
+
+// function showHide(){
+//     let mySubMenu = document.getElementById("showing-div");
+//     if(mySubMenu.style.display === "block"){
+//         mySubMenu.style.display = "none"
+//     } else{
+//      mySubMenu.style.display = "block"   
+//     }
+// }
+
+document.getElementById("showBtn").onclick = function(){showHide()}
+
+function showHide(){
+    document.getElementById("showing-div").classList.toggle("show")
+}
 
 
 
